a7bf97fc6681979bb19c3f0a431e57eb9515cd3f
dengminfeng <1015814408@qq.com>
2020-03-11 23:29:45 +0800

    feat: 添加category接口更新逻辑

--- rainDog/src/blog/model/category/category.go
+++ rainDog/src/blog/model/category/category.go
@@ -1,8 +1,19 @@
 package category
 
-import "github.com/gin-gonic/gin"
+import (
+	"github.com/gin-gonic/gin"
+)
 
-type Field struct {
+type PostField struct {
+	Name string `json:"name"`
+}
+
+type PutField struct {
+	Id string `json:"id"`
+	Name string `json:"name"`
+}
+
+type GetField struct {
 	Id string `json:"id"`
 	Name string `json:"name"`
 	Num string `json:"num"`
--- rainDog/src/blog/routes/backend.go
+++ rainDog/src/blog/routes/backend.go
@@ -10,16 +10,36 @@ import (
 func Backend(r *gin.RouterGroup) {
 	r.POST("/category", func(context *gin.Context) {
 		db, _ := helper.Db("rain_dog")
-		var field category.Field
+		var field category.PostField
 		_ = context.BindJSON(&field)
-		db.Table("category").Create(&field)
-		helper.Success(context, 200, gin.H{"id": field.Id})
+		if err := db.Table("category").Create(&field).Error; err != nil {
+			helper.Fail(context, 200, err.Error())
+			return
+		}
+		helper.Success(context, 200, gin.H{"msg": "success"})
+	})
+
+	r.PUT("/category/:id/:name", func(context *gin.Context) {
+		db, _ := helper.Db("rain_dog")
+		var putField category.PutField
+		putField.Id = context.Param("id")
+		putField.Name = context.Param("name")
+		fmt.Println(putField)
+		if err := db.Table("category").Model(&putField).Update("name", putField.Name).Error; err != nil {
+			helper.Fail(context, 200, err.Error())
+			return
+		}
+
+		helper.Success(context, 200, gin.H{"msg": "success"})
 	})
 
 	r.GET("/category", func(context *gin.Context) {
 		db, _ := helper.Db("rain_dog")
-		var fields []category.Field
-		db.Table("category").Find(&fields)
+		var fields []category.GetField
+		if err := db.Table("category").Find(&fields).Error; err != nil {
+			helper.Fail(context, 200, err.Error())
+			return
+		}
 		fmt.Println(fields)
 		helper.Success(context, 200, gin.H{"list": fields})
 	})
