adb582597f297717b6a2760cb9617b53884d566a
dmf-code <1015814408@qq.com>
2020-03-16 13:52:08 +0800

    feat: 添加文章更新和数据获取接口

--- rainDog/src/blog/model/article/article.go
+++ rainDog/src/blog/model/article/article.go
@@ -20,10 +20,10 @@ type PutField struct {
 type GetField struct {
 	Id string `json:"id"`
 	Title string `json:"title"`
-	CheckedCategorys string `json:"checked_categorys"`
-	CheckedTags string `json:"checked_tags"`
-	MdCode string `json:"md_code"`
-	HtmlCode string `json:"html_code"`
+	CheckedCategorys string `json:"checked_categorys" gorm:"column:category_ids;"`
+	CheckedTags string `json:"checked_tags" gorm:"column:tag_ids;"`
+	MdCode string `json:"mdCode"`
+	HtmlCode string `json:"htmlCode"`
 }
 
 type DeleteField struct {
--- rainDog/src/blog/routes/backend.go
+++ rainDog/src/blog/routes/backend.go
@@ -136,7 +136,14 @@ func articleGroup(r *gin.RouterGroup) {
 	})
 	
 	r.GET("/article", func(context *gin.Context) {
-		
+		db, _ := helper.Db("rain_dog")
+		var fields []article.GetField
+		if err := db.Table("article").Find(&fields).Error; err != nil {
+			helper.Fail(context, 200, "查询失败")
+			return
+		}
+
+		helper.Success(context, 200, gin.H{"list": fields})
 	})
 	
 	r.DELETE("/article/:id", func(context *gin.Context) {
