880fa00e41434466e49ba65180d082561fe106ed
dmf-code <1015814408@qq.com>
2020-03-19 21:01:03 +0800

    refeator: 将category数据库操作转到model层

--- rainDog/src/blog/model/category/category.go
+++ rainDog/src/blog/model/category/category.go
@@ -1,21 +1,79 @@
 package category
 
-type PostField struct {
-	Name string `json:"name"`
+import (
+	"blog/utils/helper"
+	"blog/utils/model"
+	"fmt"
+	"github.com/gin-gonic/gin"
+)
+
+type Category struct {
+	model.BaseModel
+	Name string `json:"name" gorm:"column:name;"`
+	Num int `json:"num"  gorm:"column:num;"`
 }
 
-type PutField struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
+func Index(ctx *gin.Context) {
+	db := helper.Db("rain_dog")
+	var fields []Category
+	if err := db.Table("tag").Find(&fields).Error; err != nil {
+		helper.Fail(ctx, "查询失败")
+		return
+	}
+
+	helper.Success(ctx, fields)
 }
 
-type GetField struct {
-	Id string `json:"id"`
-	Name string `json:"name"`
-	Num string `json:"num"`
+func Show(ctx *gin.Context) {
+	db := helper.Db("rain_dog")
+	var field Category
+	if err := db.Table("tag").Where("id = ?", ctx.Param("id")).First(&field).Error; err != nil {
+		helper.Fail(ctx, "查询失败")
+		return
+	}
+
+	helper.Success(ctx, field)
 }
 
-type DeleteField struct {
-	Id string `json:"id"`
+func Store(ctx *gin.Context) {
+	db := helper.Db("rain_dog")
+	var field Category
+	err := ctx.Bind(&field)
+	field.Num = 0
+	fmt.Println(field)
+	if err != nil {
+		helper.Fail(ctx, "绑定数据失败")
+		return
+	}
+	if err = db.Table("tag").Create(&field).Error; err != nil {
+		helper.Fail(ctx, err.Error())
+		return
+	}
+
+	helper.Success(ctx, "success")
 }
 
+func Update(ctx *gin.Context) {
+	db := helper.Db("rain_dog")
+	var filed Category
+	requestJson := helper.GetRequestJson(ctx)
+	filed.ID = helper.Str2Uint(ctx.Param("id"))
+	if err := db.Table("tag").Model(&filed).Updates(requestJson).Error; err != nil {
+		helper.Fail(ctx, err.Error())
+		return
+	}
+
+	helper.Success(ctx, "更新成功")
+}
+
+func Destroy(ctx *gin.Context) {
+	db := helper.Db("rain_dog")
+	var field Category
+	field.ID = helper.Str2Uint(ctx.Param("id"))
+	if err := db.Table("tag").Delete(&field).Error; err != nil {
+		helper.Fail(ctx, err.Error())
+		return
+	}
+
+	helper.Success(ctx, "删除成功")
+}
