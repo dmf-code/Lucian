cba27eef5ebeba0852505a9178e12837aad97338
dmf-code <1015814408@qq.com>
2020-04-08 21:00:31 +0800

    feat: 完善权限中间件

--- rainDog/src/blog/middleware/casbin.go
+++ rainDog/src/blog/middleware/casbin.go
@@ -6,6 +6,7 @@ import (
 	"fmt"
 	"github.com/gin-gonic/gin"
 	"strconv"
+	"strings"
 )
 
 // 权限中间间
@@ -21,4 +22,47 @@ func CasbinMiddleware(userId int, path string, method string) gin.HandlerFunc {
 		}
 		ctx.Next()
 	}
+}
+
+// 定义中间件跳过函数
+type SkipperFunc func(*gin.Context) bool
+
+// 检查请求路径是否包含指定前缀，如果包含则跳过
+func AllowPathPrefixSkipper(prefixes ...string) SkipperFunc {
+	return func(ctx *gin.Context) bool {
+		path := ctx.Request.URL.Path
+		pathLen := len(path)
+
+		for _, p := range prefixes {
+			if pl := len(p); pathLen >= pl && path[:pl] == p {
+				return false
+			}
+		}
+
+		return true
+	}
+}
+
+// 包含则不跳过
+func AllowPathPrefixNotSkipper(prefixes ...string) SkipperFunc {
+	return func(ctx *gin.Context) bool {
+		path := JoinRouter(ctx.Request.Method, ctx.Request.URL.Path)
+		pathLen := len(path)
+
+		for _, p := range prefixes {
+			if pl := len(p); pathLen >= pl && path[:pl] == p {
+				return true
+			}
+		}
+		return false
+	}
+}
+
+// 拼接路由
+func JoinRouter(method, path string) string {
+	if len(path) > 0 && path[0] != '/' {
+		path = "/" + path
+	}
+
+	return fmt.Sprintf("%s%s", strings.ToUpper(method), path)
 }
