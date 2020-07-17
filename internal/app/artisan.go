2862d0956622bb0231f3b1efc68f79007b7c515a
dengminfeng <1015814408@qq.com>
2020-07-17 22:22:38 +0800

    feat: 添加命令行文件夹和数据迁移命令

--- internal/app/artisan.go
+++ internal/app/artisan.go
@@ -1,7 +1,18 @@
 package main
 
-import "github.com/spf13/cobra"
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+)
 
 func main() {
+
+	var cmd = &cobra.Command{
+		Short: "welcome use cobra command",
+		Run: func(cmd *cobra.Command, args []string) {
+			fmt.Println("Hello, World!")
+		},
+	}
+
 	cmd.Execute()
 }
