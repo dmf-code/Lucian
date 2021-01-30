ef81787a96feb0163c7de01da1e1b0f2edb9bbdb
deng minfeng <1015814408@qq.com>
2021-01-30 22:27:52 +0800

    feat: 添加书签模块模型文件

--- cmd/migration.go
+++ cmd/migration.go
@@ -100,6 +100,26 @@ func initTableData() {
 	child = model.Menu{Status: 1, Memo: "", ParentID: uint64(father.ID), Url: "add", Name: "添加", Sequence: 5, Type: 4, Component: "views/admin/pages/tutorial/Add", Icon: "", OperateType: "view"}
 	helper.Db().Create(&child)
 
+	// 导航管理
+	father = model.Menu{Status: 1, Memo: "", ParentID: uint64(grandfather.ID), Url: "nav", Name: "导航管理", Sequence: 5, Type: 1, Component: "views/admin/Route", Icon: "el-icon-collection-tag", OperateType: "none"}
+	helper.Db().Create(&father)
+
+	child = model.Menu{Status: 1, Memo: "", ParentID: uint64(father.ID), Url: "list", Name: "列表", Sequence: 5, Type: 2, Component: "views/admin/pages/nav/List", Icon: "", OperateType: "view"}
+	helper.Db().Create(&child)
+
+	child = model.Menu{Status: 1, Memo: "", ParentID: uint64(father.ID), Url: "add", Name: "添加", Sequence: 5, Type: 4, Component: "views/admin/pages/nav/Add", Icon: "", OperateType: "view"}
+	helper.Db().Create(&child)
+
+	// 书签管理
+	father = model.Menu{Status: 1, Memo: "", ParentID: uint64(grandfather.ID), Url: "bookmark", Name: "书签管理", Sequence: 5, Type: 1, Component: "views/admin/Route", Icon: "el-icon-collection-tag", OperateType: "none"}
+	helper.Db().Create(&father)
+
+	child = model.Menu{Status: 1, Memo: "", ParentID: uint64(father.ID), Url: "list", Name: "列表", Sequence: 5, Type: 2, Component: "views/admin/pages/bookmark/List", Icon: "", OperateType: "view"}
+	helper.Db().Create(&child)
+
+	child = model.Menu{Status: 1, Memo: "", ParentID: uint64(father.ID), Url: "add", Name: "添加", Sequence: 5, Type: 4, Component: "views/admin/pages/bookmark/Add", Icon: "", OperateType: "view"}
+	helper.Db().Create(&child)
+
 	// 管理员账号生成
 	password, _ := bcrypt.GenerateFromPassword([]byte("123456"), bcrypt.DefaultCost)
 	account := model.Admin{Username: "admin", Password: string(password)}
@@ -112,5 +132,14 @@ func initTableData() {
 	// 账号角色关联
 	account2role := model.AdminRole{AdminId: uint64(account.ID), RoleId: uint64(role.ID)}
 	helper.Db().Create(&account2role)
+
+	nav := model.Nav{Name: "梧桐树下", Path: "/"}
+	helper.Db().Create(nav)
+
+	nav = model.Nav{Name: "教程文档", Path: "/tutorial"}
+	helper.Db().Create(nav)
+
+	nav = model.Nav{Name: "书签", Path: "/bookmark"}
+	helper.Db().Create(nav)
 }
 
